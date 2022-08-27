3e7bbdce526d7c33ffc938edbb355f78358972a2
Ztch9 <anishj545@gmail.com>
2022-08-27 13:47:32 +0530

    file

--- scripts/builder.js
+++ scripts/builder.js
@@ -1,22 +1,35 @@
 let form = document.querySelector("form");
 let signupPage = JSON.parse(localStorage.getItem("signup")) || [];
-let errorElement=document.getElementById('error');
+let err=document.getElementById('error');
+let hmm=document.getElementById('noT');
 
 form.addEventListener("submit", function (e) {
     e.preventDefault()
-    let messages=[];
-    if (signupPage.length === 0) {
-        messages.push("This is not a valid Email");
-    }
-    if(messages.length>0){
-        errorElement.innerText=messages.join(', ')
-        return;
-    }
     let obj = {
         email: form.email.value,
         password: form.password.value,
     }
-
+    if(obj.email==""){
+        err.innerText="This is not a valid email";
+        err.style.fontSize="12px";
+        err.style.color="red";
+        err.style.paddingBottom="20px";
+        err.style.fontFamily="Source Sance Pro,sans-serif";
+        document.getElementById("email").style.border="1px solid red";
+        document.getElementById("ema").style.color="red";
+        
+    }
+    if(obj.password==""){
+        hmm.innerText="Please fill in the Password";
+        hmm.style.fontSize="12px";
+        hmm.style.color="red";
+        hmm.style.paddingBottom="10px";
+        hmm.style.fontFamily="Source Sance Pro,sans-serif";
+        document.getElementById("password").style.border="1px solid red";
+        document.getElementById("woRd").style.color="red";
+        return;
+    }
+    
     let Result = false;
     signupPage.forEach(function (ele) {
 
@@ -24,11 +37,21 @@ form.addEventListener("submit", function (e) {
             Result = true;
 
             localStorage.setItem("signin", JSON.stringify(ele));
-            alert("Login Successful");
             window.location.href="index.html"
+            return;
+        }
+        if(ele.password!== obj.password){
+        hmm.innerText="Incorrect Email or Password";
+        hmm.style.fontSize="12px";
+        hmm.style.color="red";
+        hmm.style.paddingBottom="10px";
+        hmm.style.fontFamily="Source Sance Pro,sans-serif";
+        document.getElementById("password").style.border="1px solid red";
+        document.getElementById("woRd").style.color="red";
+        return;
         }
     })
-    if(Result === false){
-        alert("User Doesn't Exist");
-    }
+    // if(Result === false){
+    //     alert("User Doesn't Exist");
+    // }
 })
